2d1122b422f899fb00c38d87c0894d6d0e09c23a
kartikeybhardwaj25 <38663572+kartikeybhardwaj25@users.noreply.github.com>
2019-06-02 02:46:26 +0530

    commit

--- src/main/java/edu/org/Selenium_Jenkins.java
+++ src/main/java/edu/org/Selenium_Jenkins.java
@@ -1,7 +1,6 @@
 package edu.org;
 
-import org.testng.annotations.Test;
-import org.testng.annotations.BeforeClass;
+
 
 import java.util.concurrent.TimeUnit;
 
@@ -9,36 +8,24 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
-import org.testng.annotations.AfterClass;
 
 public class Selenium_Jenkins {
-	private WebDriver driver;
-	
-	@BeforeClass
-	  public void beforeClass() {
+	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "C:\\Selenium_Jars\\chromedriver.exe");
-		driver = new ChromeDriver();
+		WebDriver driver = new ChromeDriver();
+		
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
-	  }
-
-	  @AfterClass
-	  public void afterClass() {
-		  driver.quit();
-	  }
-	
-  @Test
-  public void verifySearchButton() {
-	  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-	  driver.get("https://www.amazon.in/");
+		
+		driver.get("https://www.amazon.in/");
 		System.out.println("successful");
 		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("PS4 Pro");
 		driver.findElement(By.xpath("//*[@id=\"nav-search\"]/form/div[2]/div/input")).click();
 		
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
+		
+
+	}
   
 
 }
